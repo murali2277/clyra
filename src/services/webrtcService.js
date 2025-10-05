8e0472489590fe870895a7fbfc11b72f5f0cc24a
Murali Kumar <0904muralikumar@gmail.com>
2025-10-05 11:17:41 +0530

    fixing deployment error

--- src/services/webrtcService.js
+++ src/services/webrtcService.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const SIGNALING_SERVER_URL = 'http://localhost:3001';
+const SIGNALING_SERVER_URL = import.meta.env.VITE_SIGNALING_SERVER_URL || 'http://localhost:3001';
 
 class WebRTCService {
   constructor() {
